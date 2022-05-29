79028c1ad5faac8d31f7cb126438f9ef695594ea
mgj9432 <101676643+mgj9432@users.noreply.github.com>
2022-05-29 15:51:20 +0100

    Update test.js

--- test.js
+++ test.js
@@ -2,5 +2,5 @@ let number = prompt("Pick a number!")
   if (number > 5) {
     alert( "That's a good number!");
   } else {
-    aler("Oh, that's a bad number!");
+    alert("Oh, that's a bad number!");
   }
