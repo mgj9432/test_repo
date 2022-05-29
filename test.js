9ea3df33c924e3495a2814c86637bd0affd2d74b
mgj9432 <101676643+mgj9432@users.noreply.github.com>
2022-05-29 17:32:17 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,4 @@
 let officialName = prompt('What is the official name of JavaScript?', '');
 
-if (age > 18) {
-  accessAllowed = true;
-} else {
-  accessAllowed = false;
-}
-
-alert(accessAllowed);
 (officialName == 'ECMAScript') ?
   alert('Right!') : alert('You don’t know? ECMAScript!');
