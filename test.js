1b86972584d57608ddd7d12ffefaf3867835da8a
mgj9432 <101676643+mgj9432@users.noreply.github.com>
2022-05-29 16:22:02 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
-let number = prompt("Pick a number!")
-  if (number > 5) {
-    alert( "That's a good number!");
-  } else {
-    alert("Oh, that's a bad number!");
-  }
+let accessAllowed;
+let age = prompt('How old are you?', '');
+
+if (age > 18) {
+  accessAllowed = true;
+} else {
+  accessAllowed = false;
+}
+
+alert(accessAllowed);
